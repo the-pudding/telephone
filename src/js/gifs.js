d4c8e3dcbd8a919f01ac29a0b378cd783668f059
Jan Diehm <jandiehm@Jans-MBP-2.lan1>
2019-02-06 17:31:42 -0600

    pulls giphy pages - need to pull count from script

--- src/js/gifs.js
+++ src/js/gifs.js
@@ -7,7 +7,10 @@ const _ = require('lodash');
 const OUT_PATH = './src/assets/data/output';
 const IN_PATH = './src/assets/data/output';
 const IN_JSON = './src/assets/data/output/billboardCharts.json'
+const allYears = ['2017', '2018'];
 const allAlbums = [];
+const artistList = [];
+const giphyUrls = [];
 
 //Pulls down html of billboard top 200 albums
 async function getBillboardHTML(year) {
@@ -48,28 +51,73 @@ function getChartData(filename) {
 	})
 }
 
-function findUniqArtists(data) {
-	//console.log(data)
-	const uniqArtists = _.uniqBy(data, 'artist')
-	const artistList = uniqArtists.map(function(obj) { return obj.artist; }).sort()
-	const json = JSON.stringify(artistList);
+function getGiphyCount(filename) {
+	const file = fs.readFileSync(`${IN_PATH}/giphy/${filename}`, 'utf-8');
+	const $ = cheerio.load(file);
 
-	fs.writeFileSync(`${OUT_PATH}/artists.json`, json);
+	const $countSpan = $('script')
+	//TO-DO: find a way to target the script
+	console.log($countSpan)
+}
+
+function findUniqArtists(data) {
+	const uniqArtists = _.uniqBy(data, 'artist')
+	artistList.push(uniqArtists.map(function(obj) { return obj.artist; }).sort())
+	//console.log(artistList.length)
+	fs.writeFileSync(`${OUT_PATH}/artists.csv`, artistList);
+}
+
+function formatGiphyUrls(data) {
+	const searchTerms = data[0]
+		.filter(d => (!d.includes('&') && !d.includes('Broadway')))
+		.map(function(artist) {
+			const term = artist.trim().replace(/\s+/g, '-').replace('.', '').replace('!', '').replace('Pnk', 'Pink')
+			return term;
+		})
+
+	const urls = searchTerms.map(function(term) {
+		return `https://giphy.com/search/${term}`
+	})
+
+	giphyUrls.push(urls)
+}
+
+async function pullGiphyHTML(link) {
+	const url = link
+	const splitz = url.split('search/')
+	const name = splitz[1];
+
+	return new Promise((resolve, reject) => {
+		request(url, (err, response, body) => {
+			fs.writeFileSync(`${OUT_PATH}/giphy/${name}.html`, body);
+		})
+	})
 }
 
 function init() {
-	getBillboardHTML(2017)
-	getBillboardHTML(2018)
+	allYears.map(getBillboardHTML)
 
 	const files = fs.readdirSync(IN_PATH).filter(d => d.includes('.html'));
 	files.map(getChartData);
 
-	const flat = [].concat(...allAlbums);
+	const tops = allAlbums.filter(d => d.rank <= 50)
+
+	const flat = [].concat(...tops);
   const json = JSON.stringify(flat);
 
 	fs.writeFileSync(`${OUT_PATH}/billboardCharts.json`, json);
 
 	findUniqArtists(flat)
+
+	formatGiphyUrls(artistList)
+	const flatGiphy = [].concat(...giphyUrls);
+	flatGiphy.map(pullGiphyHTML)
+
+	const giphyFiles = fs.readdirSync(`${IN_PATH}/giphy`).filter(d => d.includes('.html'));
+	//giphyFiles.map(getGiphyCount);
+	getGiphyCount(giphyFiles[0])
+
+
 }
 
 init();
